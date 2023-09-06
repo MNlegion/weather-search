225630feb82d39d74b400fbb68b0ab004ca6412c
MNlegion <rkreuser30@gmail.com>
2023-09-06 19:07:09 -0400

    updated logic for api call

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,1 +1,16 @@
 // logic for weather app api
+
+var getWeather = function() {
+    var apiUrl = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=e9fe7b78654e60c910997496410c10f6";
+
+    fetch(apiUrl);
+
+    // fetch(apiUrl)
+    // .then(response => response.json())
+    // .then()
+    console.log(apiUrl);
+
+
+};
+
+getWeather();
